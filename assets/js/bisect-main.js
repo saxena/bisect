e309b5ebc9805b5bbb4f1a16cbcc9c6f1970810b
Rahul Saxena <rahul.k.saxena@gmail.com>
2013-03-01 19:24:13 -0700

    Start using templates

--- assets/js/bisect-main.js
+++ assets/js/bisect-main.js
@@ -1,6 +1,8 @@
 
 "use strict";
 
+var desc_txt = "Data Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. for Top Root";
+
 var datat =
 {
     tree:
@@ -48,15 +50,78 @@ var datat =
    }
 };
 
-function render_tree(t){
-    var tmpl = Handlebars.compile($("#tmpl-tree-rooted").html());
-    $("#t-tmpl-r3-test").append(tmpl(t));
+var datac =
+    {
+        tree:
+        {
+            id :"t2",
+            head:"A Tree",
+            desc:"A Brief description of Tree"
+        },
+        nodes :
+        {
+            n0:
+            {
+                id:0,
+                head:"Top Root",
+                desc: desc_txt,
+            },
+
+            n1:
+            {
+                id:1,
+                head:"Mid Root",
+                desc:desc_txt,
+            },
+
+            n2:
+            {
+                id:2,
+                head:"Mid Root",
+                desc:desc_txt,
+            },
+
+            n3:
+            {
+                id:3,
+                head:"Bot Leaf",
+                desc:desc_txt,
+            },
+
+             n4:
+            {
+                id:4,
+                head:"Bot Leaf",
+                desc:desc_txt,
+            },
+            n5:
+            {
+                id:5,
+                head:"Bot Leaf",
+                desc:desc_txt,
+            },
+            n6:
+            {
+                id:6,
+                head:"Bot Leaf",
+                desc:desc_txt,
+            },
+
+       }
+    };
+
+function render_rtree(id, t){
+    var tmpl_tree = Handlebars.compile($("#tmpl-tree").html());
+    $(id).append(tmpl_tree(t));
 }
 
-function render_nodes_in_rows(node_list){
-
+function render_ntree(id, t){
+    var tmpl_ntree = Handlebars.compile($("#tmpl-ntree").html());
+    $(id).append(tmpl_ntree(t));
 }
 
 jQuery(document).ready(function(){
-    render_tree(datat);
+    render_rtree("#t-tmpl-r3-test", datat);
+    render_ntree("#t-tmpl-n3-test", datac);
+    //render_rtree("#t-tmpl-r31-test", datac);
 });
