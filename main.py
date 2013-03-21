ca43c1017fd728b25bffa7cbefe20bce2818315b
Rahul Saxena <rahul.k.saxena@gmail.com>
2013-03-21 05:43:52 -0600

    Implement permissions fix #2

--- main.py
+++ main.py
@@ -26,24 +26,25 @@ from google.appengine.api import users
 ####################
 def GetBT(req, tree_id):
     user = users.get_current_user()
-    logging.info('GetBT: %s', ','.join(map(str,(user,tree_id))))
+    logging.info('GetBT:%s', ','.join(map(str,(user,tree_id))))
     tree = BisectionTree.get_by_id(tree_id)
     return tree
 
-def AddBT(req):
-    user = users.get_current_user()
+def AddBT(user, req):
+    userid = user.user_id() if user else ''
     nodes = req.body
     is_public = False if req.get('is_public') is '' else True
-    tree = BisectionTree(user=user, is_public=is_public, nodes=nodes)
+    tree = BisectionTree(userid=userid, is_public=is_public, nodes=nodes)
     tree.put()
     tree_id = tree.key().id()
-    logging.info('AddBT: %s', ','.join(map(str,(user,is_public,tree_id,nodes))))
+    logging.info('AddBT:%s', ','.join(map(str,(userid,is_public,tree_id))))
     return tree_id
 
 def EditBT(req, tree_id):
-    tree = GetBT(req, tree_id)
-    if tree is not None:
-        tree.is_public = False if req.get('is_public') is '' else True
+    tree = BisectionTree.get_by_id(tree_id)
+    if tree:
+        if req.get('is_public') is not '':
+            tree.is_public = True
         tree.nodes = req.body
         tree.put()
         return tree.key().id()
@@ -51,58 +52,89 @@ def EditBT(req, tree_id):
         return None
 
 def DelBT(req, tree_id):
-    user = users.get_current_user()
-    tree = BisectionTree.get(tree_id)
-    if tree is not None:
+    tree = BisectionTree.get_by_id(tree_id)
+    if tree:
         tree.delete()
         return tree_id
     else:
         return None
 
 class BisectionTree(db.Model):
-    user = db.UserProperty(required=False, auto_current_user=False)
-    is_public = db.BooleanProperty()
-    nodes = db.TextProperty()
+    userid = db.StringProperty(required=True)
+    is_public = db.BooleanProperty(required=True)
+    nodes = db.TextProperty(required=True)
+
 ####################
-def GetUser():
+def UserCanGetBT(user, tree_id):
+    tree = BisectionTree.get_by_id(tree_id)
+    if not tree:
+        return None
+    elif tree.is_public :
+        return tree
+    elif user and tree.userid is user.user_id() :
+        return tree
+    else:
+        return None
+
+def UserCanAddBT(user, tree_id=0):
+    return True
+
+def UserCanEditBT(user, tree_id):
+    tree = BisectionTree.get_by_id(tree_id)
+    if tree and user and tree.userid is user.user_id():
+        return tree
+    else:
+        return None
+####################
+def GetUserData():
     u = users.get_current_user()
-    u_nick = u.nickname() if u else None
     u_url = users.create_logout_url("/tree") if u else users.create_login_url("/tree")
-    return (u_nick, u_url)
+    return (u, u_url)
+
+def RenderTreeTmpl(tree,u,u_url):
+    return tree_tmpl.render({
+            "jtree": tree,
+            "user": u.nickname() if u else None,
+            "user_url": u_url
+            })
 ####################
 class NewTreeHandler(webapp2.RequestHandler):
     def get(self):
-        usr,usr_url = GetUser()
-        self.response.out.write(tree_tmpl.render({
-                    "jtree": None,
-                    "user": usr,
-                    "user_url": usr_url
-                    }))
+        u,u_url = GetUserData()
+        self.response.out.write(RenderTreeTmpl(None,u,u_url))
 
     def post(self):
-        status = AddBT(self.request)
-        self.response.status_int = 202 if status else 404
-        self.response.write(json.dumps({"id":str(status)}))
+        u,u_url = GetUserData()
+        status = None
+        if UserCanAddBT(u):
+            tree_id = AddBT(u, self.request)
+            self.response.status_int = 202
+            self.response.write(json.dumps({"id":str(tree_id)}))
+        else:
+            self.response.status_int = 401
 
 class TreeHandler(webapp2.RequestHandler):
     def get(self, tree_id):
-        usr,usr_url = GetUser()
-        tree = GetBT(self.request, int(tree_id))
-        self.response.out.write(tree_tmpl.render({
-                    "jtree": tree,
-                    "user": usr,
-                    "user_url": usr_url
-                    }))
+        u,u_url = GetUserData()
+        tree = UserCanGetBT(u, int(tree_id))
+        if tree :
+            self.response.out.write(RenderTreeTmpl(tree,u,u_url))
+        else:
+            self.response.status_int = 401
 
     def put(self, tree_id):
-        status = EditBT(self.request, int(tree_id))
-        self.response.status_int = 202 if status else 404
-        self.response.write(json.dumps(status))
+        u = users.get_current_user()
+        if UserCanEditBT(u, int(tree_id)):
+            status = EditBT(self.request, int(tree_id))
+            self.response.status_int = 202 if status else 404
+            self.response.write(json.dumps(status))
 
     def delete(self, tree_id):
-        status = DelBT(self.request)
-        self.response.status_int = 202 if status else 404
-        self.response.write(json.dumps(status))
+        u = users.get_current_user()
+        if UserCanEditBT(u, int(tree_id)):
+            status = DelBT(self.request, int(tree_id))
+            self.response.status_int = 202 if status else 404
+            self.response.write(json.dumps(status))
 
 app = webapp2.WSGIApplication([
         (r'/tree/(\w+)', TreeHandler),
